877b8ab1ea7dc7f79cc5f5f3270e8a7461ce4a3b
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-04 14:22:56 +0100

    Copied code

--- Lesson 7 - References/Lesson 7 - References.cpp
+++ Lesson 7 - References/Lesson 7 - References.cpp
@@ -2,10 +2,46 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
-int main()
-{
-    return 0;
+void swap(int x, int y) {
+	int temp = x;
+	x = y;
+	y = temp;
 }
 
+void swap(int* x, int* y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void swap_ref(int& x, int& y) {
+	int temp = x;
+	x = y;
+	y = temp;
+}
+
+int main() {
+	int a, b;
+
+	cout << "Please type in a number" << "\n";
+	cin >> a;
+	cout << "Please type in another number" << "\n";
+	cin >> b;
+
+	cout << "before swap, a = " << a << " and b = " << b << "\n";
+	swap(a, b);
+	cout << "after swap (by value), a = " << a << " and b = " << b << "\n";
+	swap(&a, &b);
+	cout << "after swap by pointer reference, a = " << a << " and b = " << b << "\n";
+
+	swap_ref(a, b);
+	cout << "after swap using references, a = " << a << " and b = " << b << "\n";
+
+	int& a_ref = a;
+
+	return 0;
+}
